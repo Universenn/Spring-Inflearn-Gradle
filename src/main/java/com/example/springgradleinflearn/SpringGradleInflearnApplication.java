ec4c06c288bb917803f86654b2520cd333b79df1
Universenn <dnwndhks98@gmail.com>
2022-10-28 00:35:28 +0900

    Spring Inflearn Gradle 연습

---
+++ src/main/java/com/example/springgradleinflearn/SpringGradleInflearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgradleinflearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGradleInflearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGradleInflearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springgradleinflearn/SpringGradleInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgradleinflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGradleInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
