ebc792a8084ff9ab56b9041e347174bed69d2c3b
Universenn <dnwndhks98@gmail.com>
2022-10-29 14:39:32 +0900

    회원 가입

---
+++ src/main/java/com/example/springgradleinflearn/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package com.example.springgradleinflearn.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
