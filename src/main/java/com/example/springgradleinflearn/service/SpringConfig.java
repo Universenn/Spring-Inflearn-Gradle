3b566110d85985699c54039874e562b56d96d7f0
Universenn <dnwndhks98@gmail.com>
2022-10-29 17:07:04 +0900

    Jdbc 리포지토리 구현 1. spring.datasource.url=jdbc:h2:tcp://localhost/~/test 2. spring.datasource.driver-class-name=org.h2.Driver 3. spring.datasource.username=sa

--- src/main/java/com/example/springgradleinflearn/service/SpringConfig.java
+++ src/main/java/com/example/springgradleinflearn/service/SpringConfig.java
@@ -1,13 +1,24 @@
 package com.example.springgradleinflearn.service;
 
 import com.example.springgradleinflearn.domain.Member;
+import com.example.springgradleinflearn.repository.JdbcMemberRepository;
 import com.example.springgradleinflearn.repository.MemberRepository;
 import com.example.springgradleinflearn.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
